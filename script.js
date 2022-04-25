2caab1b3f3091d841c4d1d142e6f0cd0ef645360
Hubert Mylek <102038915+HubertMylek@users.noreply.github.com>
2022-04-25 13:45:39 +0000

    array

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function changeColor(){
   document.body.style.backgroundColor=genColor();
 }
+let myarray=[]
 function genColor(){
   let r=0,g=0,b=0;
   r=Math.round(Math.random()*255);
   g=Math.round(Math.random()*255);
   b=Math.round(Math.random()*255);
-  console.log("rgb("+r+","+g+","+b+")")
+  myarray.push(r,b,g)
+  console.log(myarray)
     return "rgb("+r+","+g+","+b+")";
 }
